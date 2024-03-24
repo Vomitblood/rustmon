9ea0055067558dc0a7b4efe315892efe2e53d6aa
Vomitblood <tohyouxuan@gmail.com>
2024-03-25 03:28:53 +0800

    debian build

---
+++ build/debian/debian.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# create a directory structure that mirrors tha final structure of the installed package
+mkdir -p pokerust/DEBIAN
+mkdir -p pokerust/usr/bin
+
+# build the executable
+cd ../..
+cargo build --release
+cd build/debian
+
+# copy the executable
+cp ../../target/release/pokerust pokerust/usr/bin/
+
+# create the control file
+touch pokerust/DEBIAN/control
+
+# edit the control file
+echo "Package: pokerust
+Version: 1.0.0
+Section: base
+Priority: optional
+Architecture: amd64
+Maintainer: Vomitblood <tohyouxuan@gmail.com>
+Description: "Pokemon Colorscripts written in Rust"" > pokerust/DEBIAN/control
+
+# actually build the thing
+echo $(pwd)
+dpkg-deb --build pokerust
